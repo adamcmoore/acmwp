1abc42aa4706d724ff38bf202b1c3a92d36a06da
Adam Moore <adam@acmoore.co.uk>
2021-07-30 10:57:40 +0100

    Fix depreciation warning with ternary

--- src/AcmWp/Request.php
+++ src/AcmWp/Request.php
@@ -15,7 +15,7 @@ Class Request
 
 	public function turboLinksHeaders()
 	{
-		$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
+		$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on" ? "s" : "");
 		$protocol = "http".$s;
 		$port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
 		$url = $protocol."://".$_SERVER['SERVER_NAME'].$port.$_SERVER['REQUEST_URI'];
